555fab00844470c43e7bad86306999847206f385
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:48:46 -0800

    Add files via upload

---
+++ Album.ts
@@ -0,0 +1,27 @@
+//Written by :  TAYYABA RAO
+// Date : 18/feb/2024
+//Roll no : 00388416
+
+// define the function make_album
+function make_album(artist: string, title: string, tracks?: number) {
+    const album: { artist:string, tittle: string, tracks?: number } ={
+        artist: artist,
+        tittle: title,
+    };
+
+    //if tracks parameter is provided, add it to the album object
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+
+    return album;
+}
+
+const album1 = make_album('Artist 1', 'Album Title 1');
+console.log(album1);
+
+const album2 = make_album('Artist 2', 'Album Title 2');
+console.log(album2);
+
+const album3 = make_album('Artist 3', 'Album Tittle 3');
+console.log(album3);
